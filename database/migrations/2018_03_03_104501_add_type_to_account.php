130cde244ecc66a1f7ea6b478d7a47ad69a6e18b
hacker-abl <ablamparas@gmail.com>
2018-04-11 15:45:47 +0800

    nullable remember_token in account table

--- database/migrations/2018_03_03_104501_add_type_to_account.php
+++ database/migrations/2018_03_03_104501_add_type_to_account.php
@@ -15,7 +15,7 @@ class AddTypeToAccount extends Migration
     {
 
          Schema::table('account', function (Blueprint $table) {
-             $table->string('remember_token');
+             $table->string('remember_token')->nullable();
         });
     }
 
